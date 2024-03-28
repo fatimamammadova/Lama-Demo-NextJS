55c08434534921a429c19820e27de331c5e20f8e
fatimamammadova <fatimammmadova@gmail.com>
2024-03-28 22:39:02 +0400

    lama-store

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -23,9 +23,9 @@ const SingleBlog = ({ params }) => {
   }, []);
 
   useEffect(() => {
-    const blogLink = document.querySelector(".navLink_active__xSnAx")
-    
-  })
+    const blogLink = document.querySelector(".Blog");
+    blogLink.classList.add("navLink_active__xSnAx");
+  });
 
   const handleDeleteBlog = async (post) => {
     const hasConfirmed = confirm("Are you sure you want to delete this blog?");
