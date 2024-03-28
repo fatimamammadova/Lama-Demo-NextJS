9ed8161f0eff10107dec64e70642d535f6b45644
fatimamammadova <fatimammmadova@gmail.com>
2024-03-28 11:47:48 +0400

    profile-page

--- src/components/BlogForm/BlogForm.jsx
+++ src/components/BlogForm/BlogForm.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import styles from "./blogForm.module.css";
 
 export const BlogForm = ({ type, post, setPost, handleSubmit, submitting }) => {
+  const currentDate = new Date();
   return (
     <div className={styles.formContainer}>
       <form className={styles.form} onSubmit={handleSubmit}>
@@ -9,20 +10,38 @@ export const BlogForm = ({ type, post, setPost, handleSubmit, submitting }) => {
           type="text"
           placeholder="Blog Title"
           defaultValue={post.title}
-          onChange={(e) => setPost({ ...post, title: e.target.value })}
+          onChange={(e) =>
+            setPost({
+              ...post,
+              title: e.target.value,
+              date: currentDate.toISOString(),
+            })
+          }
         />
         <input
           type="text"
           placeholder="Blog Image"
           defaultValue={post.image}
-          onChange={(e) => setPost({ ...post, image: e.target.value })}
+          onChange={(e) =>
+            setPost({
+              ...post,
+              image: e.target.value,
+              date: currentDate.toISOString(),
+            })
+          }
         />
         <textarea
           cols="30"
           rows="15"
           placeholder="Blog Text"
           defaultValue={post.text}
-          onChange={(e) => setPost({ ...post, text: e.target.value })}
+          onChange={(e) =>
+            setPost({
+              ...post,
+              text: e.target.value,
+              date: currentDate.toISOString(),
+            })
+          }
         ></textarea>
 
         <div className={styles.buttons}>
