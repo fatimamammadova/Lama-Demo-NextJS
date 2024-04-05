6388d629f4ae45ac1578fb9efbe2c734be30a12c
fatimamammadova <fatimammmadova@gmail.com>
2024-04-05 20:37:26 +0400

    lstore

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
 import User from "@models/user";
 import { ConnectToDB } from "@utils/database";
 function replaceAzerbaijaniLetters(text) {
